e6d47797e815985ee2f277c71778ec7185f8f26d
league <league@wts9.attlocal.net>
2018-05-19 12:33:38 -0700

    Day 1

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -1,6 +1,9 @@
 package section2;
 
 import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
@@ -8,8 +11,16 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
-		
+Robot arnold=new Robot();
+arnold.setPenWidth(15);
+arnold.setPenColor(59, 255, 237);
+arnold.penDown();
+arnold.move(100);
+arnold.turn(90);
+arnold.move(100);
+arnold.turn(90);
+arnold.move(100);
+arnold.turn(90);
+arnold.move(100);
 	}
 }
